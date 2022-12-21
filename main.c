4e184fdcc3aaaad9a67c1546ed35d7da024e98d3
lim <lbooks424@gmail.com>
2022-12-21 19:20:40 +0900

    [add] is_hittable_sphere func

--- main.c
+++ main.c
@@ -24,6 +24,77 @@ t_ray	get_camera_ray(size_t x, size_t y, t_vec camera_pos)
 	return (ray);
 }
 
+double	discriminant(t_sphere sph, t_ray ray)
+{
+	t_vec	sph_center_to_camera;
+	double	a;
+	double	b;
+	double	c;
+	double	d;
+
+	sph_center_to_camera = sub(ray.start - sph.center);
+	a = dot(ray.direction, ray.direction);
+	b = 2 * dot(ray.direction, sph_center_to_camera);
+	c = dot(sph_center_to_camera, sph_center_to_camera) - sph.radius * sph.radius;
+	d = (b * b) - 4 * a * c;
+	return (d);	
+}
+
+double	get_solution_of_quadratic_equation(t_sphere sph, t_ray ray, double d)
+{
+	t_vec	sph_center_to_camera;
+	double	a;
+	double	b;
+	double	t1;
+	double	t2;
+
+	sph_center_to_camera = sub(ray.start - sph.center);
+	a = dot(ray.direction, ray.direction);
+	b = 2 * dot(ray.direction, sph_center_to_camera);
+	if (d == 0)
+	{
+		return (-b / (2 * a));
+	}
+	else if (d > 0)
+	{
+		t1 = (-b + sqrt(d)) / (2 * a);
+		t2 = (-b - sqrt(d)) / (2 * a);
+		if (t1 > 0 && t1 < t2)
+			return (t1);
+		if (t2 > 0 && t2 < t1)
+			return (t2)
+	}
+	else
+		return (-1.0);
+}
+
+bool	is_hittable_sphere(t_sphere sph, t_ray ray, t_intersection *i_point)
+{
+	double	d;
+	double	solution;
+
+	d = discriminant(sph, ray);
+	solution = get_solution_of_quadratic_equation(d);
+	if (t > 0)
+	{
+		i_point->distance = solution;
+		i_point->pos = add(ray.start, mul(solution, ray.direction));
+		i_point->normal = normalize(sub(i_point->pos, sph.center));
+		return (true);
+	}
+	return (false);
+}
+
+bool	is_hittable(t_shape shape, t_ray ray, t_intersection *i_point)
+{
+	if (shape.type == ST_SPHERE)
+		return (is_hittable_sphere(shape, ray, i_point));
+	if (shape.type == ST_PLANE)
+		return (is_hittable_plane(shape, ray, i_point));
+//	if (shape.type == ST_SYLINDER)
+//		return (is_hittable_sylinder());
+
+}
 
 t_shape	get_nearest(t_cofig config, t_ray ray, double max_d, bool shadow)
 {
