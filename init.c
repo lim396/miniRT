494fc26190b5a9d86424b75e6bdf617de96d0f02
lim <lbooks424@gmail.com>
2022-12-22 00:50:47 +0900

    [WIP] set_config func

--- init.c
+++ init.c
@@ -1,5 +1,60 @@
 #include "minirt.h"
 
+t_config	set_ambient(char **split_line)
+{
+	size_t	i;
+
+	i = 1;
+	while (split_line[i])
+	{
+//		if (i == 1)
+			split_line[i];
+//		if (i == 2)
+//		{
+//			split(split_line[i]);
+//		}
+		i++;
+	}
+	return (config);
+}
+
+t_config	set_light(char **split_line)
+{
+	size_t	i;
+	
+	i = 1;
+	while ()
+	return (config);
+}
+
+t_config	set_camera(char **split_line)
+{
+	i = 1;
+	while ()
+	return (config);
+}
+
+t_config	set_sphere(char **split_line)
+{
+	i = 1;
+	while ()
+	return (config);
+}
+
+t_config	set_plane(char **split_line)
+{
+	i = 1;
+	while ()
+	return (config);
+}
+
+t_config	set_cylinder(char **split_line)
+{
+	i = 1;
+	while ()
+	return (config);
+}
+
 bool	set_config(t_config *config, const char *line)
 {
 	char	**split_line;
@@ -10,9 +65,22 @@ bool	set_config(t_config *config, const char *line)
 	split_line = ft_split(line, ' ');
 	if (split_line == NULL)
 		return (false);
-	while (split_line[i] != NULL)
+	if (split_line[0] != NULL)
 	{
-		
+		if (*split_line[0] == 'A')
+			config = set_ambient(split_line);
+		else if (*split_line[0] == 'L')
+			config = set_light(split_line);
+		else if (*split_line[0] == 'C')
+			config = set_camera(split_line);
+		else if (*split_line[0] == 's' && *split_line[1] == 'p')
+			config = set_sphere(split_line);
+		else if (*split_line[0] == 'p' && *split_line[1] == 'l')
+			config = set_plane(split_line);
+		else if (*split_line[0] == 'c' && *split_line[1] == 'y')
+			config = set_cylinder(split_line);
+		else
+			printf("ERROR\n");
 	}
 	return (true);
 }
