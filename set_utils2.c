7a971f7dfbc897e0ac6e2a138d79ced8e7ba724a
lim <lbooks424@gmail.com>
2023-01-06 12:42:39 +0900

    [fix] check valid elem

--- set_utils2.c
+++ set_utils2.c
@@ -22,7 +22,7 @@ double	set_fov(char *str, int *err_flag)
 		return (0);
 	}
 	fov = atod(str);
-	if (fov < 0.0 && 180.0 < fov)
+	if (fov < 0.0 || 180.0 < fov)
 		*err_flag |= INVALID_FOV;
 	return (fov);
 }
@@ -37,7 +37,7 @@ double	set_ratio(char *str, int *err_flag)
 		return (0);
 	}
 	ratio = atod(str);
-	if (ratio < 0.0 && 1.0 < ratio)
+	if (ratio < 0.0 || 1.0 < ratio)
 		*err_flag |= INVALID_RATIO;
 	return (ratio);
 }
@@ -52,7 +52,7 @@ double	set_size(char *str, int *err_flag)
 		return (0);
 	}
 	size = atod(str);
-	if (size < 0.0)
+	if (size <= 0.0)
 		*err_flag |= INVALID_SIZE;
 	return (size);
 }
